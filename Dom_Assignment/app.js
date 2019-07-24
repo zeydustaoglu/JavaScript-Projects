5c0f329c81e71f5407c258243d6591df8e5393f7
Zeyd <50028976+zeydustaoglu@users.noreply.github.com>
2019-07-24 21:15:33 +0200

    Added files

--- Dom_Assignment/app.js
+++ Dom_Assignment/app.js
@@ -4,8 +4,7 @@ var BookListOutput=['the_museum_of_innocence','the_red_haired_woman','animal_far
 ,'a_tale_of_two_cities','the_green_mile','the_little_prince','white_fang','lily_of_the_valley']
 console.log(BookListOutput)
 
-BookList = ['The Museum Of Innocence','The Red Haired Woman','Animal Farm','Nineteen Eighty Four','A Momento For Istanbul'
-,'A Tale Of Two Cities','The Green Mile','The Little Prince','White Fang','Lily Of The Valley'];
+BookList = ['The Museum Of Innocence','The Red Haired Woman','Animal Farm','Nineteen Eighty Four','A Momento For Istanbul','A Tale Of Two Cities','The Green Mile','The Little Prince','White Fang','Lily Of The Valley'];
 
 
 MenuList=['Contact','About Me','Books','Home']
@@ -16,7 +15,7 @@ const header=createAndAppend('header',document.body,null,'header')
 const logoDiv=createAndAppend('div',header,null,'logoDiv')
 const menuDiv=createAndAppend('div',header,null,'menuDiv')
 
-const logo=createAndAppend('img',logoDiv,null,'logo','/images/logo.png')
+const logo=createAndAppend('img',logoDiv,null,'logo','images/logo.png')
 const mainDiv =createAndAppend('div',document.body,null,'maindiv')
 const title=createAndAppend('h1',mainDiv,'Top 10 Books I Have Read','h1')
 
@@ -28,49 +27,49 @@ var objectBook = {
       language:'Turkish',
       author:'Orhan Pamuk',
       info: "Set in Istanbul between 1975 and today - tells the story of Kemal, the son of one of Istanbul's richest families, and of his obsessive love for a poor and distant relation, the beautiful Fusun, who is a shop-girl in a small boutique.",  
-      image: '/images/'+BookListOutput[0]+'.jpg'  
+      image: 'images/'+BookListOutput[0]+'.jpg'  
     },
     the_red_haired_woman:{
         title: BookList[1],
         language:'Turkish',
         author:'Orhan Pamuk',
         info: "This is a richly literary work: The Red-Haired Woman borrows from the tradition of the French conte philosophique and asks probing questions of ethics and of the role of art in our lives. ",  
-        image: '/images/'+BookListOutput[1]+'.jpg'  
+        image: 'images/'+BookListOutput[1]+'.jpg'  
       },
       animal_farm:{
         title: BookList[2],
         language:'English',
         author:'George Orwell',
         info: "A farm is taken over by its overworked, mistreated animals. With flaming idealism and stirring slogans, they set out to create a paradise of progress, justice, and equality. ",  
-        image: '/images/'+BookListOutput[2]+'.jpg'  
+        image: 'images/'+BookListOutput[2]+'.jpg'  
       },
       nineteen_eighty_four:{
         title: BookList[3],
         language:'English',
         author:'George Orwell',
         info: "Among the seminal texts of the 20th century, Nineteen Eighty-Four is a rare work that grows more haunting as its futuristic purgatory becomes more real.",  
-        image: '/images/'+BookListOutput[3]+'.jpg'  
+        image: 'images/'+BookListOutput[3]+'.jpg'  
       },
       a_momento_for_istanbul:{
         title: BookList[4],
         language:'Turkish',
         author:'Ahmet Umit',
         info: "A thrilling tale which moves back and forth through time, from the early days of Byzantium to the thriving metropolis of Istanbul... A corpse is discovered at the base of the statue of Atatiirk in old Istanbul, an antique coin left in its hand... ",  
-        image: '/images/'+BookListOutput[4]+'.jpg'  
+        image: 'images/'+BookListOutput[4]+'.jpg'  
       },
       a_tale_of_two_cities:{
         title: BookList[5],
         language:'Turkish',
         author:'Charles Dickens',
         info: "A Tale of Two Cities (1859) is a historical novel by Charles Dickens, set in London and Paris before and during the French Revolution. The novel tells the story of the French Doctor Manette, his 18-year-long imprisonment in the Bastille in Paris and his release to live in London with his daughter Lucie, whom he had never met. ",  
-        image: '/images/'+BookListOutput[5]+'.jpg'  
+        image: 'images/'+BookListOutput[5]+'.jpg'  
       },
       the_green_mile:{
         title: BookList[6],
         language:'English',
         author:'Stephen King',
         info: "When it first appeared, one volume per month, Stephen King's THE GREEN MILE was an unprecedented publishing triumph: all six volumes ended up on the New York Times bestseller lists—simultaneously—and delighted millions of fans the world over.",  
-        image: '/images/'+BookListOutput[6]+'.jpg'  
+        image: 'images/'+BookListOutput[6]+'.jpg'  
       },
       
       the_little_prince:{
@@ -78,28 +77,28 @@ var objectBook = {
         language:'English',
         author:'Antoine de Saint-Exupery',
         info: "'So when you look at the sky at night, it will be as if all the stars were laughing; as though in one of the stars I live and I will be laughing…You will have stars that know how to laugh!' And he laughed again.",  
-        image: '/images/'+BookListOutput[7]+'.jpg'  
+        image: 'images/'+BookListOutput[7]+'.jpg'  
       },
       white_fang:{
         title: BookList[8],
         language:'Turkish',
         author:'Jack London',
         info: "White Fang is a novel by American author Jack London (1876–1916) — and the name of the book's eponymous character, a wild wolfdog. First serialized in Outing magazine, it was published in 1906. The story details White Fang's journey to domestication in Yukon Territory and the Northwest Territories during the 1890s Klondike Gold Rush. ",  
-        image: '/images/'+BookListOutput[8]+'.jpg'  
+        image: 'images/'+BookListOutput[8]+'.jpg'  
       },
       lily_of_the_valley:{
         title: BookList[9],
         language:'English',
         author:'Balzac',
         info: "Lily of the valley, is a highly poisonouswoodland flowering plant with sweetly scented, pendent, bell-shaped white flowers borne in sprays in spring. It is native throughout the cool temperate Northern Hemisphere in Asia and Europe.",  
-        image: '/images/'+BookListOutput[9]+'.jpg'  
+        image: 'images/'+BookListOutput[9]+'.jpg'  
       },
   };
 
   const menu=renderList(MenuList,'menuDiv',menu_link)
   const books=renderList(BookList,"maindiv",BookListOutput)
 
-function renderList(array,className,linkList) {   
+function renderList(array,className) {   
 
     var ul=document.createElement('ul');
     for (let i = 0; i < array.length; i++) {
@@ -140,5 +139,5 @@ function createAndAppend(child,append,text,className,srcOfimg,href){
     return el;
 }
 const footer=createAndAppend('footer',mainDiv,null,'footer')
-// const books=renderList(BookList,"footer",BookListOutput)
+
 const footertext=createAndAppend('a',footer,'Designed by Zeyd | 2019','link',"#")
